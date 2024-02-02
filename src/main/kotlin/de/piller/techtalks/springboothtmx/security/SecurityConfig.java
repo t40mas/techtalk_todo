b046fb6e767b9da338b342e817a255cea57c520e
Thomas Fricke <thomas.fricke@piller.de>
2024-02-02 05:23:40 +0100

    feat: works but need two separate configs for html and api

--- src/main/kotlin/de/piller/techtalks/springboothtmx/security/SecurityConfig.java
+++ src/main/kotlin/de/piller/techtalks/springboothtmx/security/SecurityConfig.java
@@ -3,9 +3,9 @@ package de.piller.techtalks.springboothtmx.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Collection;
@@ -24,7 +25,8 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+@EnableMethodSecurity
+class SecurityConfig  {
 
     private static final String GROUPS = "groups";
     private static final String REALM_ACCESS_CLAIM = "realm_access";
@@ -44,7 +46,7 @@ class SecurityConfig {
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(new AntPathRequestMatcher("/todo/*/*"))
+                .requestMatchers("/todo")
                 .hasRole("user")
                 .requestMatchers(new AntPathRequestMatcher("/"))
                 .permitAll()
@@ -54,10 +56,6 @@ class SecurityConfig {
                 .jwt(Customizer.withDefaults()));
         http.oauth2Login(Customizer.withDefaults())
                 .logout(logout -> logout.addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/"));
-        http.cors(Customizer.withDefaults());
-        http.oauth2Login(Customizer.withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 
