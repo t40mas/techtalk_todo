4d92b6805979c7b02fa2df89138f797a6d46b5ee
Thomas Fricke <thomas.fricke@piller.de>
2024-02-07 12:20:26 +0100

    feat: works but custom spring security configuration

--- src/main/kotlin/de/piller/techtalks/springboothtmx/security/SecurityConfig.java
+++ src/main/kotlin/de/piller/techtalks/springboothtmx/security/SecurityConfig.java
@@ -6,29 +6,35 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    private final Filter springSecurityFilterChain;
-
-    public SecurityConfig(@Qualifier("springSecurityFilterChain") Filter springSecurityFilterChain) {
-        this.springSecurityFilterChain = springSecurityFilterChain;
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/todo/*"))
+                .hasRole("user")
+                .requestMatchers(new AntPathRequestMatcher("/"))
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+        http.oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults()));
+        http.oauth2Login(Customizer.withDefaults());
+        return http.build();
     }
 
-    public void getFilters() {
-        FilterChainProxy filterChainProxy = (FilterChainProxy) springSecurityFilterChain;
-        List<SecurityFilterChain> list = filterChainProxy.getFilterChains();
-        list.stream()
-                .flatMap(chain -> chain.getFilters().stream())
-                .forEach(filter -> System.out.println(filter.getClass()));
-    }
 
 }
 
