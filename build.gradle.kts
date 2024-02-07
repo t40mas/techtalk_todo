ef50a12856782fc8f1e54fdd4cc94ad9ac292aae
Thomas Fricke <thomas.fricke@piller.de>
2024-02-07 12:22:24 +0100

    chore: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
