573289b71fe645ab79fa91803674eb65a5835f6b
Thomas Fricke <thomas.fricke@piller.de>
2024-02-07 12:45:46 +0100

    debug: add principal

--- src/main/kotlin/de/piller/techtalks/springboothtmx/todo/controller/TodoItemController.kt
+++ src/main/kotlin/de/piller/techtalks/springboothtmx/todo/controller/TodoItemController.kt
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
+import java.security.Principal
 import java.util.stream.Collectors
 
 
@@ -17,7 +18,7 @@ import java.util.stream.Collectors
 class TodoItemController(var repository: TodoItemRepository) {
 
     @GetMapping
-    fun index(model: Model): String {
+    fun index(model: Model, principal: Principal): String {
         println("used: index")
         addAttributesForIndex(model, ListFilter.ALL)
         return "index"
