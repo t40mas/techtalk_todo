fb386222fd7484238fdde40b358b2dee016a7925
Thomas Fricke <thomas.fricke@piller.de>
2024-02-07 14:44:56 +0100

    chore: reformat

--- src/main/kotlin/de/piller/techtalks/springboothtmx/SpringBootHtmxApplication.kt
+++ src/main/kotlin/de/piller/techtalks/springboothtmx/SpringBootHtmxApplication.kt
@@ -2,15 +2,6 @@ package de.piller.techtalks.springboothtmx
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.config.Customizer
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
-
 
 @SpringBootApplication
 class SpringBootHtmxApplication
