bbdb46eb337a427b7339b94dd25769691a759a5c
Thomas Fricke <6321074+t40mas@users.noreply.github.com>
2024-03-01 00:16:10 +0100

    chore: cleanup

--- src/main/kotlin/de/piller/techtalks/springboothtmx/security/SecurityConfig.java
+++ src/main/kotlin/de/piller/techtalks/springboothtmx/security/SecurityConfig.java
@@ -1,20 +1,12 @@
 package de.piller.techtalks.springboothtmx.security;
 
-import jakarta.servlet.Filter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
